1c3ceec854184678a10e9e4a42949c4646ae99e8
saito gen <gensaito921@gmail.com>
2023-06-27 11:14:31 +0900

    test time

--- hayaokizamurai/App.js
+++ hayaokizamurai/App.js
@@ -7,6 +7,7 @@ const App = () => {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* テストです！ */}
     </View>
   );
 };
